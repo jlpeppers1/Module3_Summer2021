6daf1cc7a726fc7d333cedf267ba69cbc55bae9b
jlpeppers1 <Monkey#12fuckgithubshit>
2021-05-31 21:35:17 -0500

    submitting just pseudocode

---
+++ topic1/ifelif.py
@@ -0,0 +1,39 @@
+"""
+Program: ifelif.py
+Author: Joseph Peppers
+Last date modified: 05/25/2021
+TODO
+"""
+
+#In the shell or in a text editor, write a program that
+# asks for the user to sign up for Programmer's Toolkit Monthly Subscription Box.
+# They must select level of membership they want.
+# Each month is something different, t-shirts, stickers, figurines, even programming books!
+#The levels are the following:
+#Platinum  60$
+#Gold      50$
+#Silver    40$
+#Bronze    30$
+#Free Trial    Free $0.00
+#Write an if statement that prints the cost for each of the level. Platinum is $60, each level below is 10 dollars cheaper, and the free trail is free.
+
+#pseudocode
+
+#tell the user what programmers toolkit is/contains
+    #print something like these
+        #"Sign up for Programmer's Toolkit Monthly Subscription Box"
+        #"Each month is something different, t-shirts, stickers, figurines, even programming books!"
+        #print possible levels they can select from
+#ask user to select a membership level
+    #grab user input somehow
+    #cast this to something we can validate with basd on how we set up the level selection
+
+#based on what level the user selected, print the cost of that level
+    # if level is platinum
+        #print something like "cost is $60 for Platinum"
+    # else if level is gold
+        #print something like "cost is $50 for Gold"
+    #... etc
+    #example 'ford' == 'ford' True
+        #x = 'ford'
+        # if (x == 'ford')
