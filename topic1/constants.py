e725191a19a14b4beea741cfa85c8d838024de11
jlpeppers1 <Monkey#12fuckgithubshit>
2021-06-01 19:48:31 -0500

    adding lecture notes

---
+++ topic1/constants.py
@@ -0,0 +1,8 @@
+"""
+Program: constants.py
+Author: Joseph Peppers
+Last date modified: 05/25/2021
+TODO
+"""
+
+TAX=.06
---
+++ topic1/coupon_calculations.py
@@ -0,0 +1,10 @@
+"""
+Program: coupon_calculations.py
+Author: Joseph Peppers
+Last date modified: 05/25/2021
+TODO
+"""
+
+import constants
+
+
